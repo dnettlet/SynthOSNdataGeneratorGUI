10329e8ae847a3a06046fad3c65510e5009904b0
Marc Canal <44952259+mcanal8@users.noreply.github.com>
2019-04-24 21:38:55 +0200

    Implementing xml file config import

--- src/genDataNOapplication/Utils/FileUtils.java
+++ src/genDataNOapplication/Utils/FileUtils.java
@@ -10,8 +10,37 @@ import org.w3c.dom.Element;
 public class FileUtils {
 
    public static void load(File file) {
+	   
+	   try {
+		   DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	         Document doc = dBuilder.parse(file);
+	         doc.getDocumentElement().normalize();
+	         System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 
-      try {
+    		 Element eElement = (Element) doc.getElementsByTagName("inputFiles").item(0);
+    		 System.out.println("InputFile1: " + eElement.getElementsByTagName("inputFile1").item(0).getTextContent());
+    		 System.out.println("InputFile2: " + eElement.getElementsByTagName("inputFile2").item(0).getTextContent());
+
+
+        	 Element outFilesElem = (Element) doc.getElementsByTagName("outputFiles").item(0);
+	         System.out.println("OutFile: " + outFilesElem.getElementsByTagName("outFile").item(0).getTextContent());
+	         System.out.println("OutgFile: " + outFilesElem.getElementsByTagName("outgFile").item(0).getTextContent());
+	         System.out.println("Out1File: " + outFilesElem.getElementsByTagName("out1File").item(0).getTextContent());
+	         System.out.println("Out2File: " + outFilesElem.getElementsByTagName("out2File").item(0).getTextContent());
+	         
+
+        	 Element varElem = (Element) doc.getElementsByTagName("vars").item(0);
+        	 System.out.println("Numcommunities: " + varElem.getElementsByTagName("numCommunities").item(0).getTextContent());
+        	 System.out.println("SeedSize: " + varElem.getElementsByTagName("seedSize").item(0).getTextContent());
+        	 System.out.println("Randomness: " + varElem.getElementsByTagName("randomness").item(0).getTextContent());
+	         
+	         
+	   } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+
+      /*try {
          DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
          DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
          Document doc = dBuilder.parse(file);
@@ -52,6 +81,6 @@ public class FileUtils {
          }
       } catch (Exception e) {
          e.printStackTrace();
-      }
+      }*/
    }
 }
